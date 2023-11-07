38947d9264c01085248864a3b324df1d6c42025f
arunbk83 <arunbk.kmg@gmail.com>
2023-11-07 14:29:21 +0530

    new codebase committing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultSidebarComponent } from './containers/default-sidebar/default-sidebar.component';
+import { DefaultHeaderComponent } from './containers/default-header/default-header.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultSidebarComponent,
+    DefaultHeaderComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
